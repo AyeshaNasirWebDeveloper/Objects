23f11e81cf63c831a14d2e2150df82184b630e29
AyeshaNasirWebDeveloper <ayeshanasir806@gmail.com>
2024-02-26 20:33:24 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,53 @@
+/* Non Premitive Data Types
+Objects */
+
+
+const student = {
+    fullName: "Ayesha Nasir",
+    marks: 93,
+    grade: "A+",
+    isPass: true,
+}
+
+console.log(student);
+
+// OR
+
+console.log(student.fullName);
+console.log(student.marks);
+console.log(student.grade);
+console.log(student.isPass);
+
+
+student = {
+    fullName: "Uroosa Nasir",
+    marks: 95,
+    grade: "A+",
+    isPass: true,
+}
+
+console.log(student);
+
+// OR
+
+console.log(student["fullName"]);
+console.log(student["marks"]);
+console.log(student["grade"]);
+console.log(student["isPass"]);
+
+
+student = {
+    fullName: "Muhammad Nasir",
+    marks: 99,
+    grade: "A+",
+    isPass: true,
+}
+
+console.log(student);
+
+// OR
+
+console.log(student.fullName);
+console.log(student.marks);
+console.log(student.grade);
+console.log(student.isPass);
